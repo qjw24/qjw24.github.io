e8d8cb23c469bed14240375d2228663a28fabbd8
XHPix <k13794687769@163.com>
2024-06-26 19:50:08 +0800

    Site updated: 2024-06-26 19:50:08

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  if(!localStorage.getItem('agreed')) {
-    alert('请阅读并同意我们的条款协议');
-    localStorage.setItem('agreed', 'true');
-  }
-});
